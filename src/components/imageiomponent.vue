75bd3f06bb190f6365abf3c06dbb4d2d2c3caf67
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-21 14:08:01 +0000

    New file name

---
+++ src/components/imageiomponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    srcUrl: {
+        type: String
+    }
+})
+</script>
+
+<template>
+    <div class="overflow-hidden">
+        <img :src="srcUrl" alt="profile picture" class="rounded-full">
+    </div>
+</template>
