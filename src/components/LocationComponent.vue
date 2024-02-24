439412b68c70ea44324d9531af821078997f9a73
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-24 11:20:37 +0000

    Add Location component

---
+++ src/components/LocationComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    location: Object
+})
+
+const comp_location = computed(() => {
+    let { city, country } = props.location
+    return `${city}, ${country}`
+})
+
+</script>
+
+<template>
+    <div>
+        <p>{{ comp_location }}</p>
+    </div>
+</template>
