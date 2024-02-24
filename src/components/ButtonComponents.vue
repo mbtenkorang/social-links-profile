f469b771032dd528769570a4b8a0c0af49b808e6
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-24 11:21:41 +0000

    Add Link components

--- src/components/ButtonComponents.vue
+++ src/components/ButtonComponents.vue
@@ -1,2 +1,16 @@
-<script setup></script>
-<template></template>
+<script setup>
+const props = defineProps({
+    social_links: Array
+})
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in social_links" :key="index">
+                <a :href="item.websiteUrl" target="_blank">
+                    {{ item.websiteName}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
