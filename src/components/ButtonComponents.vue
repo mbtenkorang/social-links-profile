64fed611e3ed4a3a09e5ee2d3d9e1f9e9d135cd2
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-25 19:39:14 +0000

    Remove margin for last child element

--- src/components/ButtonComponents.vue
+++ src/components/ButtonComponents.vue
@@ -5,8 +5,8 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="w-full last:mb-0">
-        <a class="block w-full mb-5 py-3 text-center border border-neutral-grey bg-neutral-grey rounded-lg transition-colors ease-in-out hover:bg-primary-green hover:text-neutral-grey hover:duration-400 focus:bg-primary-green focus:text-neutral-grey"
+    <div class="w-full">
+        <a class="block w-full mb-5 last:mb-0 py-3 text-center border border-neutral-grey bg-neutral-grey rounded-lg transition-colors ease-in-out hover:bg-primary-green hover:text-neutral-grey hover:duration-400 focus:bg-primary-green focus:text-neutral-grey"
             v-for="(item,index) in social_links" :href="item.websiteUrl" target="_blank" :key="index">
             <span class="font-semibold text-sm">
                 {{ item.websiteName}}
