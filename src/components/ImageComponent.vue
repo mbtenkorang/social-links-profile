51cec78f3c0f6a66a880e521f6ad0aef71b33264
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-24 09:25:09 +0000

    Added required attribute to url prop

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -1,11 +1,12 @@
 <script setup>
 const props = defineProps({
     srcUrl: {
-        type: String
+        type: String,
+        required: true
     }
 })
 </script>
 
 <template>
-    <img :src=" srcUrl" alt="profile picture" class="rounded-full w-[35%] md:w-2/6 mx-auto">
+    <img :src="srcUrl" alt="profile picture" class="rounded-full w-[35%] md:w-2/6 mx-auto">
 </template>
