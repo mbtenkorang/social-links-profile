bf7903eefdba45c21ab5e5fae34e9e0585de35f7
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-31 20:40:19 +0000

    Changed username to userName

--- src/components/NameComponent.vue
+++ src/components/NameComponent.vue
@@ -1,9 +1,9 @@
 <script setup>
 const props = defineProps({
-    username: { type: String },
+    userName: { type: String },
 })
 </script>
 
 <template>
-    <h1 class="font-bold text-2xl text-center mt-4">{{ username }}</h1>
+    <h1 class="font-bold text-2xl text-center mt-4">{{ userName }}</h1>
 </template>
